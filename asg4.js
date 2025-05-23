f47717f6155c6e3323da0a07330d3e6833df0501
saemlee734 <130793493+saemlee734@users.noreply.github.com>
2025-05-23 15:06:28 -0700

    Update asg4.js

--- asg4.js
+++ asg4.js
@@ -2,6 +2,30 @@
 // saemlee@ucsc.edu
 // asg4.js
 
+function drawTriangle3DUVNormal(vbuffer, uvbuffer, nbuffer, vertices, uv, normals) {
+  if (!gl) return;
+
+  // Vertex buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  // UV buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, uvbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
+  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_UV);
+
+  // Normal buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, nbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.DYNAMIC_DRAW);
+  gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Normal);
+
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
+}
+
 var VSHADER_SOURCE = `
   precision mediump float;
   
